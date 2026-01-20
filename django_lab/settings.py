f731be04ac1beb8fa047f2f6038d64ddda4f521b
chetank <chetank@clavax.us>
2026-01-20 19:13:30 +0530

    update import file

--- django_lab/settings.py
+++ django_lab/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1-hipaurrti9idt)-)o!l24ai65ykw3^+#d)^a)@9*91!d5v6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
