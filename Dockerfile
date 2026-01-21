e3b65f07d6cba705cf25c0392f08ed92cad59a2d
chetank <chetank@clavax.us>
2026-01-21 17:02:00 +0530

    gunicorn command update

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ RUN python -m pip install --upgrade pip && pip install -r requirements.txt
 
 COPY . .
 
+RUN python manage.py collectstatic --noinput
+
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "django_lab.wsgi:application"]
 
 #CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- django_lab/settings.py
+++ django_lab/settings.py
@@ -147,6 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/app/staticfiles"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
