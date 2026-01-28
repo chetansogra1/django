53b953488342c56082f3b54111922def3087d428
chetank <chetank@clavax.us>
2026-01-28 15:32:55 +0530

    update settings.py file

--- django_lab/settings.py
+++ django_lab/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1-hipaurrti9idt)-)o!l24ai65ykw3^+#d)^a)@9*91!d5v6!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
